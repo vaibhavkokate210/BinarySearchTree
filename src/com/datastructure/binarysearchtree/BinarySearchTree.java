9b8616385ef03dcfadd9c36409ed0b5c73f12d28
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-29 12:12:33 +0530

    [Vaibhav] added BST is created and 30,70 added after 56

--- src/com/datastructure/binarysearchtree/BinarySearchTree.java
+++ src/com/datastructure/binarysearchtree/BinarySearchTree.java
@@ -7,5 +7,50 @@ public class BinarySearchTree
 	{
 		this.root=null;
 	}
-	
+	public void insert(int key)
+	{
+		Node node=new Node(key);
+		if(root==null)
+		{
+			root=node;
+		}
+		else
+		{
+			Node current=root;
+			Node parent;
+			while(true)
+			{
+				parent=current;
+				if(key<current.key)
+				{
+					current=current.left;
+					if(current==null)
+					{
+						parent.left=node;
+						return;
+					}
+				}
+				else
+				{
+					current=current.right;
+					if(current==null)
+					{
+						parent.right=node;
+						return;
+					}
+				}
+			}
+		}
+	}
+	public void display()
+	{
+		display(root);
+	}
+	private void display(Node root){
+		if(root!=null){
+			display(root.left);
+			System.out.print(" " + root.key);
+			display(root.right);
+		}
+	}
 }
--- src/com/datastructure/binarysearchtree/BinarySearchTreeRunner.java
+++ src/com/datastructure/binarysearchtree/BinarySearchTreeRunner.java
@@ -4,7 +4,11 @@ public class BinarySearchTreeRunner
 {
 	public static void main(String[] args) 
 	{
-		
+		BinarySearchTree bst=new BinarySearchTree();
+		bst.insert(56);
+		bst.insert(30);
+		bst.insert(70);
+		bst.display();
 	}
 
 }
