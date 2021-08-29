28bd8b6efcde69791b028a9858836438e3f836db
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-29 12:17:08 +0530

    [Vaibhav] added complete binary search tree is created

--- src/com/datastructure/binarysearchtree/BinarySearchTreeRunner.java
+++ src/com/datastructure/binarysearchtree/BinarySearchTreeRunner.java
@@ -8,6 +8,16 @@ public class BinarySearchTreeRunner
 		bst.insert(56);
 		bst.insert(30);
 		bst.insert(70);
+		bst.insert(22);
+		bst.insert(40);
+		bst.insert(60);
+		bst.insert(95);
+		bst.insert(11);
+		bst.insert(65);
+		bst.insert(3);
+		bst.insert(16);
+		bst.insert(63);
+		bst.insert(67);
 		bst.display();
 	}
 
